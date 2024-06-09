ddcd05fb05003b1aa17df187b0894c991695c5b4
nekrassov01 <unclek62@gmail.com>
2024-06-10 00:20:30 +0900

    chore: fix typo

--- test/cdk-bedrock.test.ts
+++ test/cdk-bedrock.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkLineBedrock from '../lib/cdk-bedrock-stack';
+// import * as Bedrock from '../lib/stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-bedrock-stack.ts
 test("SQS Queue Created", () => {
   //   const app = new cdk.App();
   //     // WHEN
-  //   const stack = new CdkLineBedrock.CdkLineBedrockStack(app, 'MyTestStack');
+  //   const stack = new Bedrock.BedrockStack(app, 'MyTestStack');
   //     // THEN
   //   const template = Template.fromStack(stack);
   //   template.hasResourceProperties('AWS::SQS::Queue', {
